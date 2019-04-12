decceb905ec8ee0a035317f6fd67e6702fd2dcd9
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-04-12 13:23:56 -0400

    Set up ajax call for jokes and successfully logged data

--- Project_1/js/app.js
+++ Project_1/js/app.js
@@ -1,3 +1,12 @@
 $(() => {
-  console.log('yodawg');
+  $.ajax({
+    url:'https://official-joke-api.appspot.com/random_ten'
+  }).then(
+    (data)=>{
+      console.log(data);
+    },
+    ()=>{
+      console.log('request not found');
+    }
+  )
 })
