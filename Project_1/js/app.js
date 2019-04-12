2b59db06e3d75c8239427f40872e78294587fff2
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-04-12 13:44:47 -0400

    Finished data function to dynamically load all 10 joke divs at once to html

--- Project_1/js/app.js
+++ Project_1/js/app.js
@@ -3,7 +3,16 @@ $(() => {
     url:'https://official-joke-api.appspot.com/random_ten'
   }).then(
     (data)=>{
-      console.log(data);
+      for(i = 0; i < data.length; i++) {
+        const $div = $('<div>').addClass('joke');
+        const $question = $('<h2>').addClass('question').text(data[i].setup);
+        const $answerBtn = $('<button>').addClass('answerBtn').text('Answer');
+        const $answer = $('<h3>').addClass('answer').text(data[i].punchline);
+        $div.append($question);
+        $div.append($answerBtn);
+        $div.append($answer);
+        $('.carousel').append($div);
+      }
     },
     ()=>{
       console.log('request not found');
