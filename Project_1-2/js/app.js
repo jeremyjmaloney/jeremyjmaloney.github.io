d4fd88d080a44f37d9b5d7484f821b36946362d1
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-04-15 19:46:02 -0400

    Connected to api, parsed the data, and able to log it

--- Project_1-2/js/app.js
+++ Project_1-2/js/app.js
@@ -1,3 +1,30 @@
 $(()=> {
-  
+  $('.submit').on('click', (event) => {
+    let $myLoc = $('.location').val();
+    // console.log($myLoc);
+    $.ajax({
+      url:`https://cors.io/?https://www.metaweather.com/api/location/search/?query=${$myLoc}`
+    }).then(
+      (data)=>{
+        let parsedData = JSON.parse(data);
+        let woeidNumber = parsedData[0].woeid;
+        console.log(woeidNumber);
+        $.ajax({
+          url:`https://cors.io/?https://www.metaweather.com/api/location/${woeidNumber}`
+        }).then(
+          (data)=>{
+            let parsedData = JSON.parse(data);
+            // console.log(parsedData.consolidated_weather[0].applicable_date);
+            let days = parsedData.consolidated_weather;
+            for(let i = 0; i < days; i++) {
+              
+            }
+          }
+        )
+      },
+      ()=>{
+        console.log('request not found');
+      }
+    )
+  })
 })
