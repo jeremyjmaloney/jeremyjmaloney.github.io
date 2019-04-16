4990a0cf22b6a48530a2bb8add9fd60716970544
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-04-15 20:43:40 -0400

    Able to seach city, and populate html with data

--- Project_1-2/js/app.js
+++ Project_1-2/js/app.js
@@ -1,7 +1,9 @@
 $(()=> {
   $('.submit').on('click', (event) => {
+    $('.week').empty();
+    const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let $dotw = new Date().getDay();
     let $myLoc = $('.location').val();
-    // console.log($myLoc);
     $.ajax({
       url:`https://cors.io/?https://www.metaweather.com/api/location/search/?query=${$myLoc}`
     }).then(
@@ -16,8 +18,18 @@ $(()=> {
             let parsedData = JSON.parse(data);
             // console.log(parsedData.consolidated_weather[0].applicable_date);
             let days = parsedData.consolidated_weather;
-            for(let i = 0; i < days; i++) {
-              
+            for(let i = 0; i < days.length; i++) {
+              let $weatherDiv = $('<div>');
+              let $date = days[i].applicable_date;
+              let $photoCode = days[i].weather_state_abbr;
+              let $photo = $('<img>').attr('src', `https://www.metaweather.com/static/img/weather/png/64/${$photoCode}.png`).addClass('icon');
+              let $curTemp = Math.round(((days[i].the_temp * 9/5) + 32));
+              $weatherDiv.append($dotw).text(weekday[$dotw]);
+              $weatherDiv.append($date);
+              $weatherDiv.append($photo);
+              $weatherDiv.append($curTemp);
+              $('.week').append($weatherDiv);
+              $dotw++;
             }
           }
         )
