c23b3b2972d7477d19e1e97695fb0264755fe34e
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-06-12 15:36:55 -0400

    Able to keep track of scrolled amount

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 class App extends React.Component {
-  handleScroll = (event) => {
-    let scrollAmount = event.deltaY;
-    let count = 0;
-    console.log('I scrolled');
-    count++;
-    console.log(count);
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  handleScroll = event => {
+    let element = event.target
+    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+      this.setState({
+        count: this.state.count + 1
+      })
+      console.log(this.state.count);
     }
   }
   render() {
     return (
-      <div className="content-container" onWheel={this.handleScroll}>
+      <div className="container" onWheel={this.handleScroll}>
         <p>lsdkfaj dsfaj sdlfajfljsdlf jasfljsdfljopweifpoweiufpoweifu pawoeifu apoeifua apsodifu pasodiuf paodsiuf poasudf poaidsuf posiduf poausd pfoiasdufpo isudpfoiasud fpoisdufpoiw eurpoqiweurpoqewi rupqoweiruer uqpeowirupeowiru eowiru eow roi euwproiu qweoiru qoweiur pwoeqi r</p>
         <p>lsdkfaj dsfaj sdlfajfljsdlf jasfljsdfljopweifpoweiufpoweifu pawoeifu apoeifua apsodifu pasodiuf paodsiuf poasudf poaidsuf posiduf poausd pfoiasdufpo isudpfoiasud fpoisdufpoiw eurpoqiweurpoqewi rupqoweiruer uqpeowirupeowiru eowiru eow roi euwproiu qweoiru qoweiur pwoeqi r</p>
         <p>lsdkfaj dsfaj sdlfajfljsdlf jasfljsdfljopweifpoweiufpoweifu pawoeifu apoeifua apsodifu pasodiuf paodsiuf poasudf poaidsuf posiduf poausd pfoiasdufpo isudpfoiasud fpoisdufpoiw eurpoqiweurpoqewi rupqoweiruer uqpeowirupeowiru eowiru eow roi euwproiu qweoiru qoweiur pwoeqi r</p>
