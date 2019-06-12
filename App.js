5443c7fb636435aaa0f20482978c9820f4badacb
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-06-12 13:27:35 -0400

    Added annonymous function

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
-var winheight, docheight, trackLength, throttlescroll
+()=>{
+  var winheight, docheight, trackLength, throttlescroll
 
-function getmeasurements(){
-    winheight= window.innerHeight || (document.documentElement || document.body).clientHeight
-    docheight = getDocHeight()
-    trackLength = docheight - winheight
+  function getmeasurements(){
+      winheight= window.innerHeight || (document.documentElement || document.body).clientHeight
+      docheight = getDocHeight()
+      trackLength = docheight - winheight
+  }
+
+  function amountscrolled(){
+      var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+      var pctScrolled = Math.floor(scrollTop/trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+      console.log(pctScrolled + '% scrolled')
+  }
+
+  getmeasurements()
+
+  window.addEventListener("resize", function(){
+      getmeasurements()
+  }, false)
+
+  window.addEventListener("scroll", function(){
+      clearTimeout(throttlescroll)
+          throttlescroll = setTimeout(function(){ // throttle code inside scroll to once every 50 milliseconds
+          amountscrolled()
+      }, 50)
+  }, false)
 }
-
-function amountscrolled(){
-    var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
-    var pctScrolled = Math.floor(scrollTop/trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
-    console.log(pctScrolled + '% scrolled')
-}
-
-getmeasurements()
-
-window.addEventListener("resize", function(){
-    getmeasurements()
-}, false)
-
-window.addEventListener("scroll", function(){
-    clearTimeout(throttlescroll)
-        throttlescroll = setTimeout(function(){ // throttle code inside scroll to once every 50 milliseconds
-        amountscrolled()
-    }, 50)
-}, false)
